c6c780ed965bb8c42973461ba9d9c5b0e2baec18
Gianluca Daffre <gianluca.daffre@noseryoung.ch>
2022-12-15 21:37:39 +0100

    config depending on locale

--- i18n/config.ts
+++ i18n/config.ts
@@ -1,14 +1,31 @@
 import i18n from "i18next";
 import translation from "./de/translation.json";
 import { initReactI18next } from "react-i18next";
-
+import { NativeModules, Platform } from "react-native";
 export const resources = {
   de: {
     translation,
   },
 } as const;
 
-i18n.use(initReactI18next).init({
+const locale =
+  Platform.OS === "ios"
+    ? NativeModules.SettingsManager.settings.AppleLocale
+    : NativeModules.I18nManager.localeIdentifier;
+
+i18n.init({
+  resources,
+  lng: locale.substring(0, 2),
+  fallbackLng: "en", // use en if detected lng is not available
+
+  keySeparator: false, // we do not use keys in form messages.welcome
+
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+});
+
+https: i18n.use(initReactI18next).init({
   lng: "de",
   interpolation: {
     escapeValue: false, // not needed for react as it escapes by default
