823fa593cee6d7f76b7354157b9cceea783ca1b1
SkyBollin <sky.bollin@noseryoung.ch>
2022-12-16 09:25:43 +0100

    fix for storage

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import {
-  Text,
-  MD3LightTheme as DefaultTheme,
-  Provider as PaperProvider,
   IconButton,
-  MD3Colors,
-  TextInput,
+  MD3Colors, MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider, Text
 } from "react-native-paper";
 import StorageService from "./services/StorageService";
 
@@ -22,14 +18,16 @@ const theme = {
 
 export default function App() {
   const [likes, setLikes] = useState(0);
+
   useEffect(() => {
-    return () => {
-      StorageService.getData("@mykey").then((value) => {
-        console.log("value = ", value);
-        if (value) setLikes(parseInt(value));
-      });
-    };
+    StorageService.getData("@mykey").then((value) => {
+      if (value) setLikes(Number(value));
+    });
   }, []);
+
+  useEffect(() => {
+    StorageService.storeData("@mykey", likes.toString());
+  }, [likes])
 
   return (
     <PaperProvider theme={theme}>
@@ -39,10 +37,7 @@ export default function App() {
           icon="thumb-up"
           iconColor={MD3Colors.primary0}
           size={40}
-          onPress={() => {
-            setLikes(likes + 1);
-            StorageService.storeData("@mykey", likes.toString());
-          }}
+          onPress={() => { setLikes(likes + 1); }}
         />
       </View>
     </PaperProvider>
